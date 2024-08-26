da1792573592051d472467505abd42daa936cbaf
Leosouthey <621862169@qq.com>
2024-08-27 02:37:45 +0800

    refactor: Update input component styles
    This commit updates the styles of the input component to improve its visual appearance and user experience. Changes include adjusting the border, background color, padding, and shadow effects. The updates ensure a consistent design across the application and enhance the overall look and feel of the input component.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-[color,box-shadow] file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:shadow-[inset_0px_2px_8px_0px_rgba(34,40,49,0.15),inset_0px_-2px_8px_0px_rgba(34,40,49,0.15)] dark:focus-visible:[box-shadow:inset_0px_0px_8px_0px_rgba(255,255,255,0.2)] disabled:cursor-not-allowed disabled:opacity-50",
           "shadow-[inset_0px_2px_4px_0px_rgba(34,40,49,0.1),inset_0px_-2px_4px_0px_rgba(34,40,49,0.1)] dark:[box-shadow:inset_0px_0px_6px_0px_rgba(255,255,255,0.1)]",
           className
         )}
